b32a56c6085c98b2c81a111379e66e16d5d8af91
João André Anunciação <joao.anunciacao@venturus.org.br>
2024-05-10 11:45:56 -0300

    exercicio 1 e ex extra 1

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -1,8 +1,21 @@
 /// <reference types="Cypress" />
 
 describe('Central de Atendimento ao Cliente TAT', function() {
-    it('verifica o título da aplicação', function() {
+    beforeEach(function() {
         cy.visit('./src/index.html')
+    })
+    
+    it('verifica o título da aplicação', function() {
         cy.title().should('be.equal', 'Central de Atendimento ao Cliente TAT')
+    })
+
+    it.only('Preenche os campos obrigatórios e envia o formulário', function() {
+        const longText = 'Text lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum'
+        cy.get('#firstName').type('João')
+        cy.get('#lastName').type('Anunciação')
+        cy.get('#email').type('joaobot@hotmail.com')
+        cy.get('#open-text-area')
+        .type(longText, { delay: 0 })
+        cy.get('button[type="submit"]').click()
     })
   })
