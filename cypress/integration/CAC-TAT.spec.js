71401fccf155b4201f96906241b049b7bfb5f42e
João André Anunciação <joao.anunciacao@venturus.org.br>
2024-05-14 10:52:24 -0300

    exercicio extra 5 resolvido

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -33,7 +33,7 @@ describe('Central de Atendimento ao Cliente TAT', function() {
     })
 
     // Exercício extra 4
-    it.only('Exibe mensagem de erro quando o telefone se torna obrigatório mas não é preenchido antes do envio', function() {
+    it('Exibe mensagem de erro quando o telefone se torna obrigatório mas não é preenchido antes do envio', function() {
         cy.get('#firstName').type('João')
         cy.get('#lastName').type('Anunciação')
         cy.get('#email').type('joaobot@hotmail.com')
@@ -41,5 +41,20 @@ describe('Central de Atendimento ao Cliente TAT', function() {
         cy.get('#phone-checkbox').check()
         cy.get('button[type="submit"]').click()
         cy.get('.error').should('be.visible')
+    })
+
+    // Exercício extra 5
+    it.only('preenche e limpa os campos nome, sobrenome, email e telefone', function() {
+        cy.get('#firstName').type('João').should('have.value', 'João')
+        cy.get('#lastName').type('Anunciação').should('have.value', 'Anunciação')
+        cy.get('#email').type('joaobot@hotmail.com').should('have.value', 'joaobot@hotmail.com')
+        cy.get('#phone').type('19998979668').should('have.value', '19998979668')
+        cy.get('#open-text-area').type('Teste').should('have.value', 'Teste')
+
+        cy.get('#firstName').clear().should('have.value', '')
+        cy.get('#lastName').clear('').should('have.value', '')
+        cy.get('#email').clear('').should('have.value', '')
+        cy.get('#phone').clear('').should('have.value', '')
+        cy.get('#open-text-area').clear('').should('have.value', '')
     })
   })
